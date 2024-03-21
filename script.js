ffa21c042559cfa151e0fd59c681bc5eeb0ac9ae
Patrick Riedinger <patried7@gmail.com>
2024-03-21 13:37:19 -0600

    added a dollar sign favicon

--- script.js
+++ script.js
@@ -58,60 +58,61 @@ const getRandomEmployee = function(employeesArray) {
 
 // Display employee data in an HTML table
 const displayEmployees = function(employeesArray) {
-  // Get the employee table
-  const employeeTable = document.querySelector('#employee-table');
-
-  // Clear the employee table
-  employeeTable.innerHTML = '';
-
-  // Loop through the employee data and create a row for each employee
-  for (let i = 0; i < employeesArray.length; i++) {
-    const currentEmployee = employeesArray[i];
-
-    const newTableRow = document.createElement("tr");
-
-    const firstNameCell = document.createElement("td");
-    firstNameCell.textContent = currentEmployee.firstName;
-    newTableRow.append(firstNameCell);
-
-    const lastNameCell = document.createElement("td");
-    lastNameCell.textContent = currentEmployee.lastName;
-    newTableRow.append(lastNameCell);
-
-    const salaryCell = document.createElement("td");
-    // Format the salary as currency
-    salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US",{
-      style:"currency",
-      currency:"USD"
-    });
-
-    newTableRow.append(salaryCell);
-
-    employeeTable.append(newTableRow);
-  }
-}
-
-const trackEmployeeData = function() {
-  const employees = collectEmployees();
-
-  console.table(employees);
-
-  displayAverageSalary(employees);
-
-  console.log('==============================');
-
-  getRandomEmployee(employees);
-
-  employees.sort(function(a,b) {
-    if (a.lastName < b.lastName) {
-      return -1;
-    } else {
-      return 1;
+    // Get the employee table
+    const employeeTable = document.querySelector('#employee-table');
+  
+    // Clear the employee table
+    employeeTable.innerHTML = '';
+  
+    // Loop through the employee data and create a row for each employee
+    for (let i = 0; i < employeesArray.length; i++) {
+      const currentEmployee = employeesArray[i];
+  
+      const newTableRow = document.createElement("tr");
+  
+      const firstNameCell = document.createElement("td");
+      firstNameCell.textContent = currentEmployee.firstName;
+      newTableRow.append(firstNameCell);
+  
+      const lastNameCell = document.createElement("td");
+      lastNameCell.textContent = currentEmployee.lastName;
+      newTableRow.append(lastNameCell);
+  
+      const salaryCell = document.createElement("td");
+      // Format the salary as currency
+      salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US",{
+        style:"currency",
+        currency:"USD"
+      });
+  
+      newTableRow.append(salaryCell);
+  
+      employeeTable.append(newTableRow);
     }
-  });
-
-  displayEmployees(employees);
-}
-
-// Add event listener to 'Add Employees' button
-addEmployeesBtn.addEventListener('click', trackEmployeeData);
+  }
+  
+  const trackEmployeeData = function() {
+    const employees = collectEmployees();
+  
+    console.table(employees);
+  
+    displayAverageSalary(employees);
+  
+    console.log('==============================');
+  
+    getRandomEmployee(employees);
+  
+    employees.sort(function(a,b) {
+      if (a.lastName < b.lastName) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+  
+    displayEmployees(employees);
+  }
+  
+  // Add event listener to 'Add Employees' button
+  addEmployeesBtn.addEventListener('click', trackEmployeeData);
+  
